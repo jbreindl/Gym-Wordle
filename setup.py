b6a68693c6b213bd6d81c3ccaba855ed491c6aca
David Kraemer <david.kraemer@stonybrook.edu>
2022-02-03 15:49:19 -0500

    Package information

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name='WordleBot',
+    version='0.1',
+    description='RL environment for playing Wordle',
+    author='David Kraemer',
+    author_email='david.kraemer@stonybrook.edu',
+    packages=['wordlebot'], 
+)
