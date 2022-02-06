45d414fbb9e3a4821df6c18ecfc11bbb64dda34a
David Kraemer <david.kraemer@stonybrook.edu>
2022-02-06 13:52:59 -0500

    Added documentation and rewrote the state/action space representation for stable_baselines compatibility

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='gym_wordle',
-    version='0.1.0',
+    version='0.1.2',
     author='David Kraemer',
     author_email='david.kraemer@stonybrook.edu',
     description='OpenAI gym environment for training agents on Wordle',
