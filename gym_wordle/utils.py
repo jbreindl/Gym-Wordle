7ba2b36cced9d6267b2b8a8e468e932b9ab50f58
David Kraemer <david.kraemer@stonybrook.edu>
2022-02-06 10:04:14 -0500

    Documentation and some refactoring

--- gym_wordle/utils.py
+++ gym_wordle/utils.py
@@ -9,25 +9,48 @@ _char_d = {c: i for i, c in enumerate(_chars)}
 
 
 def to_english(array: npt.NDArray[np.int64]) -> str:
-    """
-    Converts a numpy integer array into a corresponding English string.
+    """Converts a numpy integer array into a corresponding English string.
 
-    Params
-    ------
-    array: 
+    Args:
+        array: Word in array (int) form. It is assumed that each integer in the
+          array is between 0,...,26 (inclusive).
+
+    Returns:
+        A (lowercase) string representation of the word.    
     """
     return ''.join(_chars[i] for i in array)
 
 
 def to_array(word: str) -> npt.NDArray[np.int64]:
-    """
-    Converts a string of characters into a corresponding numpy array
+    """Converts a string of characters into a corresponding numpy array.
+
+    Args:
+        word: Word in string form. It is assumed that each character in the
+          string is either an empty space ' ' or lowercase alphabetical
+          character.
+
+    Returns:
+        An array representation of the word.
     """
     return np.array([_char_d[c] for c in word])
 
 
-def get_words(category: str, build: bool=False) -> npt.NDArray[np.int64]:
-    """
+def get_words(category: str, build: bool =False) -> npt.NDArray[np.int64]:
+    """Loads a list of words in array form. 
+
+    If specified, this will recompute the list from the human-readable list of
+    words, and save the results in array form.
+
+    Args:
+        category: Either 'guess' or 'solution', which corresponds to the list
+          of acceptable guess words and the list of acceptable solution words.
+        build: If True, recomputes and saves the array-version of the computed
+          list for future access.
+
+    Returns:
+        An array representation of the list of words specified by the category.
+        This array has two dimensions, and the number of columns is fixed at
+        five.
     """
     assert category in {'guess', 'solution'}
     
@@ -43,29 +66,29 @@ def get_words(category: str, build: bool=False) -> npt.NDArray[np.int64]:
 
 
 def play():
+    """Play Wordle yourself!"""
     import gym
     import gym_wordle
     
-    env = gym.make('Wordle-v0')
+    env = gym.make('Wordle-v0')  # load the environment
     
     env.reset()
+    solution = to_english(env.solution).upper()  # no peeking!
+
     done = False
     
     while not done:
-        env.render()
-        good_guess = False
-    
-        while not good_guess:
-    
-            guess = input('Guess: ').lower()
+        action = to_array('too long')
+
+        # in general, the environment won't be forgiving if you input an
+        # invalid word, but for this function I want to let you screw up user
+        # input without consequence, so just loops until valid input is taken.
+        while not env.action_space.contains(action):
+            guess = input('Guess: ')
             action = to_array(guess)
     
-            if env.action_space.contains(action):
-                good_guess = True
-    
         state, reward, done, info = env.step(action)
+        env.render()
     
-    env.render()
-    
-    print(f"The word was {to_english(env.solution).upper()}")
+    print(f"The word was {solution}")
 
